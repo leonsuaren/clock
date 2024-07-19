824be051b6dc8ef6d24914dd2e714e26cbe3ee69
Leon Suarez <leonsua@gmail.com>
2024-07-19 19:20:23 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import HomePage from './main-page/MainPage.tsx';
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/main-page/MainPage.tsx
@@ -0,0 +1,9 @@
+import { type FC } from 'react';
+
+const MainPage: FC = () => {
+  return (
+    <h1>Main Page</h1>
+  )
+}
+
+export default MainPage;
