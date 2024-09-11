b4386fce4ada34b8a649a1f17c6698d7ceacec51
Leon Suarez <leonsua@gmail.com>
2024-09-11 19:31:11 -0400

    hours, minutes and seconds working

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,22 +1,43 @@
-import { type FC } from 'react';
+import { type FC, useState, useEffect } from "react";
 
-import { ClockStyled } from './styled.ts';
+import { ClockStyled } from "./styled.ts";
 
 const Clock: FC = () => {
-  const d = new Date();
-  let time = d.toLocaleTimeString();
-  let hours = time.slice(0, 1);
-  let minutes = time.slice(2, 4);
-  let seconds = time.slice(5, 7);
-  let meridiano = time.slice(8 , 10);
-  
+  const [ time, setTime ] = useState({
+    hours: new Date().getHours(),
+    minutes: new Date().getMinutes(),
+    seconds: new Date().getSeconds()
+  });
+
+  useEffect(() => {
+     setInterval(() => {
+      const date = new Date();
+      setTime({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds()
+      });
+    }, 1000);
+  }, []);
+
+  const onConvertToString = (number: number) => {
+    return number.toLocaleString('en-US', {
+      minimumIntegerDigits: 2
+    })
+  }
 
   return (
     <>
-    <ClockStyled>{hours} : {minutes} : {seconds} {meridiano}</ClockStyled>
+      <ClockStyled>
+        <span>{onConvertToString(time.hours)}</span>
+        <span> : </span>
+        <span>{onConvertToString(time.minutes)}</span>
+        <span> : </span>
+        <span>{onConvertToString(time.seconds)}</span>
+        <span>{time.hours >= 12 ? ' PM' : ' AM'}</span>
+      </ClockStyled>
     </>
-
-  )
-}
+  );
+};
 
 export default Clock;
